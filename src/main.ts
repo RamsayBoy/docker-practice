5d07b259e6932fe5a9461df564ba90f1aa83c8f7
Ramsay <38114695+RamsayBoy@users.noreply.github.com>
2022-07-26 15:55:24 +0200

    Add max line length .editorconfig

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 
 const app = express();
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 3000;
 
 dotenv.config();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+	res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`http://${host}:${port}/`);
+	console.log(`http://${host}:${port}/`);
 });
